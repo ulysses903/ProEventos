9a18b8527ec3eead54344d3c919abb9ffb579acc
ulysses903 <ulysses903@gmail.com>
2022-01-31 13:49:02 -0300

    Inicializando git

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+                EventoId = 1,
+                DataEvento = DateTime.Today.ToString(),
+                ImagemURL = "Teste",
+                Local = "12",
+                Lote = "Lote",
+                QtdPessoas = 7,
+                Tema = "Folia"
+            };
+        }
+    }
+}
